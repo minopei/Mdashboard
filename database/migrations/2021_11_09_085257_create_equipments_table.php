f2d617e8b1cceb85743529f7cee3fdb2bdf4408e
k1655 <yiru_wang@adimmune.com.tw>
2021-12-14 11:04:19 +0800

    migrate table(plants.productionline.equipments

---
+++ database/migrations/2021_11_09_085257_create_equipments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('productionline_id')->references('id')->on('productionline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+}
